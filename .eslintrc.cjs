ff92d44b18f50afb3433949aa8f61711b0292909
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2022-01-30 10:30:41 +0100

    Add puppeteer & eslint dependencies through yarn

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: "eslint:all",
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    "array-element-newline": ["error", "consistent"],
+    "function-call-argument-newline": ["error", "consistent"],
+    "indent": ["error", 2],
+    "no-magic-numbers": "off",
+    "no-warning-comments": "off",
+    "object-curly-spacing": ["error", "always"],
+    "one-var": "off",
+    "padded-blocks": ["error", { blocks: "never" }],
+    "quote-props": ["error", "consistent-as-needed"],
+    "semi": ["error", "never"]
+  }
+}
