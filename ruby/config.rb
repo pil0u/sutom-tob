de1aa953ad844ac5f199d362885fcbb7657ead39
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2022-03-02 15:31:56 +0100

    Ajout des derniers mots du jour

--- ruby/config.rb
+++ ruby/config.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-MODE = 'player' # ['sutom', 'player', 'bot', 'benchmark']
+MODE = 'sutom' # ['sutom', 'player', 'bot', 'benchmark']
 
 # sutom
 DERNIERE_VERSION_BOT = 3
-JOUR_SUTOM = 52
+JOUR_SUTOM = 54
 
 # player | bot
 LE_MOT = nil # 'GARAGE'
